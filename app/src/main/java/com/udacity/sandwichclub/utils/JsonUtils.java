7cd21f46f363e0a12af11c137ac88690bc466ac4
cnote06 <cls3b86@gmail.com>
2018-05-18 17:09:30 -0700

    Project 1 Initial Commit. Sandwich app. Google nanodegree

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,45 @@
 package com.udacity.sandwichclub.utils;
 
+import android.util.Log;
+
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Collections;
+import java.util.List;
+
+import static android.content.ContentValues.TAG;
 
 public class JsonUtils {
 
-    public static Sandwich parseSandwichJson(String json) {
+    public static Sandwich parseSandwichJson(String json) throws JSONException {
+        int position = 0;
+        int i = position;
+
+        try {
+            JSONObject jsonData = new JSONObject(json);
+            JSONArray sandwich_detailsArray = jsonData.getJSONArray("sandwich_details");
+
+            for (i = 0; i < sandwich_detailsArray.length(); i++) {
+                JSONObject jsonSandwich = sandwich_detailsArray.getJSONObject(i);
+                String mainName = jsonSandwich.getString("mainName");
+                List<String> alsoKnownAs = Collections.singletonList(jsonSandwich.getString("alsoKnownAs"));
+                String placeOfOrigin = jsonSandwich.getString("placeOfOrigin");
+                String description = jsonSandwich.getString("description");
+                String image = jsonSandwich.getString("image");
+                List<String> ingredients = Collections.singletonList(jsonSandwich.getString("ingredients"));
+
+                Log.d(TAG, "Data download");
+            }
+        } catch(JSONException jsone) {
+            jsone.printStackTrace();
+            Log.e(TAG, "error processing parseSandwichJson: " + jsone.getMessage());
+
+        }
         return null;
     }
 }
+
