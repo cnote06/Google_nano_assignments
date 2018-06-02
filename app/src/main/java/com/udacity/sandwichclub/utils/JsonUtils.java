63a6571ae853ecc8e5d9bd040f6256ba26c869b5
cnote06 <cls3b86@gmail.com>
2018-06-01 18:37:02 -0700

    Project 1 Initial Commit. Sandwich app. Google nanodegree

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,6 +2,8 @@ package com.udacity.sandwichclub.utils;
 
 import android.util.Log;
 
+import com.udacity.sandwichclub.model.Sandwich;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -13,30 +15,29 @@ import static android.content.ContentValues.TAG;
 
 public class JsonUtils {
 
-    public static parseSandwichJson(String json) throws JSONException {
+    public static Sandwich parseSandwichJson(String json) throws JSONException {
         int position = 0;
         int i = position;
 
-        try {
-            JSONObject jsonData = new JSONObject(json);
-            JSONArray sandwich_detailsArray = jsonData.getJSONArray("sandwich_details");
+        JSONObject jsonData = new JSONObject(json);
+        JSONArray sandwich_detailsArray = jsonData.getJSONArray("sandwich_details");
 
-            for (i = 0; i < sandwich_detailsArray.length(); i++) {
-                JSONObject jsonSandwich = sandwich_detailsArray.getJSONObject(i);
-                String mainName = jsonSandwich.getString("mainName");
-                List<String> alsoKnownAs = Collections.singletonList(jsonSandwich.getString("alsoKnownAs"));
-                String placeOfOrigin = jsonSandwich.getString("placeOfOrigin");
-                String description = jsonSandwich.getString("description");
-                String image = jsonSandwich.getString("image");
-                List<String> ingredients = Collections.singletonList(jsonSandwich.getString("ingredients"));
+        for (i = 0; i < sandwich_detailsArray.length(); i++) {
+            JSONObject jsonSandwich = sandwich_detailsArray.getJSONObject(i);
+            String mainName = jsonSandwich.getString("mainName");
+            List<String> alsoKnownAs = Collections.singletonList(jsonSandwich.getString("alsoKnownAs"));
+            String placeOfOrigin = jsonSandwich.getString("placeOfOrigin");
+            String description = jsonSandwich.getString("description");
+            String image = jsonSandwich.getString("image");
+            List<String> ingredients = Collections.singletonList(jsonSandwich.getString("ingredients"));
 
-                Log.d(TAG, "Data download");
-            }
-        } catch(JSONException json) {
-            json.printStackTrace();
-            Log.e(TAG, "error processing parseSandwichJson: " + json.getMessage());
+            Log.d(TAG, "Data download");
+
+
         }
-        return null;
-    }
-}
+        return parseSandwichJson(json);
 
+
+    }
+
+}
